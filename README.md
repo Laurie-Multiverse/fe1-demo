38bc7387ee7c785a59e122bfe453e830ca8be87d
Laurie Corrin <laurie.corrin@multiverse.io>
2022-10-20 09:55:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Starter Project for the Front End, Week 1 Demo
+## After cloning this repo:
+###
+### npm install
+###
+## To run your project:
+###
+### npm run start-dev
+###
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+export function App() {
+
+  return (
+    <Container>
+      <h1>Hello, world!</h1>
+    </Container>
+  )
+}
